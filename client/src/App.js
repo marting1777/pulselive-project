72fafa7f3e1b76495c8d7462e2cf72d7a2ec9990
Martin Galli <martin.ggalli@gmail.com>
2019-09-24 15:02:59 -0300

    Select showing dynamic data

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 
 import Option from './components/UI/Option/Option'
+import Card from './components/UI/Card/Card'
 
 class App extends Component {
 
     state = {
         response: '',
-        players: ''
+        players: '',
+        option: '',
+        card: '',
     }
 
     componentDidMount() {
@@ -21,13 +24,34 @@ class App extends Component {
         const body = await response.json()
         if (response.status !== 200) throw Error(body.message)
         this.setState({ response: body })
-        
+        this.setState({ card: body[0] })
         this.createPlayersHandler()
+    }
+
+    onOptionChange = event => {
+        let playerSelected = event.target.value
+        this.setState({ option: playerSelected })
+
+        let cardsList = this.state.response
+        let selectedCard
+        let playerSelectedId = parseInt(playerSelected, 10)
+        cardsList.forEach(card => {
+            if (card && card.player && playerSelectedId) {
+                if (card.player.id === playerSelectedId) {
+                    selectedCard = card
+                }
+            }
+        })
+        this.setState({ card: selectedCard })
     }
 
     createPlayersHandler = () => {
         const playersData = this.state.response.map(players => {
-            return <Option key={players.player.id} value={players.player.id}>{players.player.name.first} {players.player.name.last}</Option>
+            return <Option 
+                        key={players.player.id} 
+                        value={players.player.id}>
+                            {players.player.name.first} {players.player.name.last}
+                    </Option>
         })
         return this.setState({ players: playersData })
     }
@@ -38,11 +62,12 @@ class App extends Component {
             <div className="App">
                 <section className="CardArea">
                     <div className="FormSelect">
-                        <select>
+                        <select onChange={this.onOptionChange}>
                             <option value="Select a Player...">Select a Player...</option>
                             {this.state.players}
                         </select>
                     </div>
+                    <Card fullPlayer={this.state.card}/>
                 </section>
             </div>
         )
--- client/src/components/UI/Card/Card.js
+++ client/src/components/UI/Card/Card.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import './Card.css'
+
+import Aux from '../../../containers/Auxiliar/Auxiliar'
 
 class Card extends Component {
+
+    constructor (props) {
+        super(props)
+    }
+
     render() {
         return (
-            <div>
-                <h1>{this.props.name} {this.props.surname}</h1>
+            <div className="Card">
+                {this.props.fullPlayer ? 
+                <Aux>
+                    <div className="ImageContainer">
+                        <img />
+                    </div>
+                    <div className="StatsContainer">
+                
+                        <h2>{this.props.fullPlayer.player.name.first}</h2>
+                        <p>Defender</p>
+                        <img className="TeamLogo"/>
+                        <ul className="StatsList">
+                            <li className="Stat">Appearances <span className="StatValue">80</span></li>
+                            <li className="Stat">Goals <span className="StatValue">5</span></li>
+                            <li className="Stat">Assits <span className="StatValue">2</span></li>
+                            <li className="Stat">Goals per match <span className="StatValue">0.06</span></li>
+                            <li className="Stat">Passes per minute <span className="StatValue">0.26</span></li>
+                        </ul>
+                    </div>  
+                </Aux>
+                : <p>Select A Player to Be Rendered</p> }
             </div>
         )
     }
